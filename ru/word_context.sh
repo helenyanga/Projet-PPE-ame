46dad6ae48921efcd82337a0277f4c36d721c068
Paul <bellevillepaul03@gmail.com>
2025-12-31 17:41:40 +0100

    finished processing ru2, will continue next year

--- ru/word_context.sh
+++ ru/word_context.sh
@@ -7,7 +7,7 @@ write_file_arrays()
     local export_folder=$2
     local filename="$(get_filename "$input_file")"
     # Possible export folder = txt/processed/$filename
-    folder_exist "$export_folder"
+    path_exists "$export_folder"
     # \p{L} matches any unicode character 
     # (?<!\p{L}) no letter before the match
     # (?!\p{L}) no letter after the match
@@ -34,21 +34,35 @@ process_files_with_spinner()
     local source_folder_path=$1 # txt/original/ru2
     local export_folder_base=$2 # txt/processed
 
+    message="Starting file processing"
+    printf "%s" "$message"
+
+    for i in {1..3}; do
+        printf "."
+        sleep 0.4
+    done
+    printf "\rFile processing started!\n"
+
     local source_folder="$source_folder_path/*"
     local spinner="/-\|"
     local spin_index=0
     local count_files
     count_files=$(folder_length "$source_folder_path")
     local count_file=0
+    
+    local folder_name="$(get_folder_name $source_folder)"
+    local export_folder_base="$export_folder_base/$folder_name"
+    echo $export_folder_base
+    path_exists "$export_folder_base"
 
     for file in $source_folder; do
         local filename
         filename="$(get_filename "$file")"
-        write_file_arrays "$file" "$export_folder_base/$filename"
+        write_file_arrays "$file" "$export_folder_base"
 
         # Update spinner
         local spin_char="${spinner:spin_index:1}"
-        printf "\rConverting files (%d/%d) %s" "$count_file" "$count_files" "$spin_char"
+        printf "\rProcessing files (%d/%d) %s" "$count_file" "$count_files" "$spin_char"
 
         # Move to next spinner
         spin_index=$(( (spin_index + 1) % 4 ))
@@ -56,8 +70,8 @@ process_files_with_spinner()
         count_file=$((count_file+1))
     done
 
-    printf "\rAll files from %s have been converted to array files\n" "$source_folder_path"
+    printf "\rAll files from %s have been processed\n" "$source_folder_path"
 }
 
 process_files_with_spinner "txt/original/ru2" "txt/processed"
-process_files_with_spinner "txt/original/ru1" "txt/processed"
+#process_files_with_spinner "txt/original/ru1" "txt/processed"
