50b015b45c2a64e946ddf777eea41199bd2e798b
Paul <bellevillepaul03@gmail.com>
2025-12-31 01:32:57 +0100

    functions created and modified, too much bash for today, now need to create a dialog for smoother use and make a pattern for ru1

--- ru/word_context.sh
+++ ru/word_context.sh
@@ -3,19 +3,19 @@ source ../utils.sh # Import useful functions
 
 write_file_arrays()
 {
-    input_file=$1
-    export_folder=$2
-    filename="$(get_filename "$input_file")"
+    local input_file=$1
+    local export_folder=$2
+    local filename="$(get_filename "$input_file")"
     # Possible export folder = txt/processed/$filename
     folder_exist "$export_folder"
     # \p{L} matches any unicode character 
     # (?<!\p{L}) no letter before the match
     # (?!\p{L}) no letter after the match
-    pattern='(?<!\p{L})дух(а|у|ом|е|и|ов|ам|ами|ах)?(?!\p{L})'
+    local pattern='(?<!\p{L})дух(а|у|ом|е|и|ов|ам|ами|ах)?(?!\p{L})'
     
     # Gets the word in the file 1 line of context after and before
-    count=0
-    current_export_file="$export_folder/$filename-$count.txt"
+    local count=0
+    local current_export_file="$export_folder/$filename-$count.txt"
 
     grep -nPi -A1 -B1 --color "$pattern" "$input_file" | while IFS= read -r line; do
         if [[ "$line" == "--" ]]; then
@@ -29,28 +29,35 @@ write_file_arrays()
 
 }
 
-# Spinner characters
-spinner="/-\|"
-spin_index=0
-count_files="$(folder_length "txt/original/ru2/")"
+process_files_with_spinner() 
+{
+    local source_folder_path=$1 # txt/original/ru2
+    local export_folder_base=$2 # txt/processed
 
-count_file=0
-source_folder=txt/original/ru2/*
-count_files="$(folder_length "txt/original/ru2/")"
+    local source_folder="$source_folder_path/*"
+    local spinner="/-\|"
+    local spin_index=0
+    local count_files
+    count_files=$(folder_length "$source_folder_path")
+    local count_file=0
 
-for file in $source_folder; do
-    filename="$(get_filename "$file")"
-    write_file_arrays $file "txt/processed/$filename"
+    for file in $source_folder; do
+        local filename
+        filename="$(get_filename "$file")"
+        write_file_arrays "$file" "$export_folder_base/$filename"
 
-    # Update spinner
-    spin_char="${spinner:spin_index:1}"
-    printf "\rConverting urls (%d/%d) %s" "$count_file" "$count_files" "$spin_char"
+        # Update spinner
+        local spin_char="${spinner:spin_index:1}"
+        printf "\rConverting files (%d/%d) %s" "$count_file" "$count_files" "$spin_char"
 
-    # Move to next spinner
-    spin_index=$(( (spin_index + 1) % 4 ))
+        # Move to next spinner
+        spin_index=$(( (spin_index + 1) % 4 ))
 
-    count_file=$((count_file+1))
-done 
-printf "\rAll files from %s have been converted to array files\n" \
-        "$source_folder"
+        count_file=$((count_file+1))
+    done
 
+    printf "\rAll files from %s have been converted to array files\n" "$source_folder_path"
+}
+
+process_files_with_spinner "txt/original/ru2" "txt/processed"
+process_files_with_spinner "txt/original/ru1" "txt/processed"
