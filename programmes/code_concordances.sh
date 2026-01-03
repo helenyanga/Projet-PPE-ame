a18c5b87b5e8e5167b7cf07c83f6a53f5c3cf561
HelenaYang <helena.yang@sorbonne-nouvelle.fr>
2026-01-03 22:11:51 +0100

    test_concordances1

--- programmes/code_concordances.sh
+++ programmes/code_concordances.sh
@@ -13,10 +13,14 @@ then
 	exit 1
 fi
 
-N=1
-for fichier in "$fichier_context"/fr-*.txt
+for fichier in $(ls "$fichier_context"/fr-*.txt)
 do
+    nom_fichier=$(basename "$fichier" .txt)
+    N=$(echo "nom_fichier" | sed 's/^fr-//')
+
     fichier_html_concordance="$fichier_concordance/fr-${N}.html"
+
+    echo "Traitement des fichiers : $nom_fichier → fr-${N}.html"
 
     echo -e "<html>
             <head>
@@ -30,30 +34,23 @@ do
                         <th>Contexte droit</th>
                     </tr>" > "$fichier_html_concordance"
 
-    phrase_mot=$(egrep -a -o -i '(\w+\W){0,5}\b(Â|â)me(s)?\b(\W+\w){0,5})' "$fichier")
-
-    while IFS= read -r line
+    grep -o -i -E '.{0,50}\b[aâ]mes?\b.{0,50}' "$fichier" | while IFS= read -r line
     do
-        if echo -e "$line" | egrep -iq '\b(Â|â)me(s)?\b'
-        then
-            mot=$(echo "$line" | egrep -o -i "\bâme(s)?\b")
-            contexte_gauche=$(echo -e "$line" | sed -E 's/(.*)\bâme(s)?\b.*/\1/I')
-            contexte_droit=$(echo -e "$line" | sed -E 's/.*\bâme(s)?\b(.*)/\1/I')
+        mot=$(echo "$line" | grep -o -i -E "\b[ÂAaâ]me(s)?\b" | head -1)
+        contexte_gauche=$(echo "$line" | sed -E "s/^(.*)\b[aâ]mes?\b.*$/\1/I")                contexte_droit=$(echo "$line" | sed -E "s/^.*\b[aâ]mes?\b(.*)$/\1/I")
 
         echo -e "  <tr>
                     <td>$contexte_gauche</td>
                     <td>$mot</td>
                     <td>$contexte_droit</td>
-                </tr>" >> "$fichier_html_concordance"
-        fi
-    done <<< "$phrase_mot"
+                   </tr>" >> "$fichier_html_concordance"
 
+    done
 
     echo -e "          </table>
                 </body>
             </html>" >> "$fichier_html_concordance"
 
-    N=$((N + 1))
 done
 
 echo "Concordancier terminés."
